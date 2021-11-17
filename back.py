a6700374c5999998dfc0d2d79b0c3fb35517765d
derick <derick.wu@qq.com>
2021-11-17 14:36:00 +0800

    fix bugs

--- back.py
+++ back.py
@@ -4,12 +4,14 @@ from flask_cors import CORS
 import os
 import re
 import json
-from datetime import datetime, timedelta
+from datetime import timedelta
 
+port = 3333
 prefix = '/home/yz/test/'
 video_exts = frozenset(['.mp4'])
 
-app = Flask(__name__, static_url_path='/',
+app = Flask(__name__,
+            static_url_path='/',
             static_folder='build')
 CORS(app)
 
@@ -53,31 +55,31 @@ def get_chunk(full_path: str, byte1=None, byte2=None):
 
 @app.route('/api/video', methods=['GET'])
 def api_file():
-    match request.method:
-        case 'GET':
-            range_header = request.headers.get('Range', None)
-            file = request.args.get('file', None)
-            byte1, byte2 = 0, None
-            if range_header:
-                match = re.search(r'(\d+)-(\d*)', range_header)
-                groups = match.groups()
+    if request.method == 'GET':
+        range_header = request.headers.get('Range', None)
+        file = '.' + request.args.get('file', None)
+        byte1, byte2 = 0, None
+        if range_header:
+            match = re.search(r'(\d+)-(\d*)', range_header)
+            groups = match.groups()
 
-                if groups[0]:
-                    byte1 = int(groups[0])
-                if groups[1]:
-                    byte2 = int(groups[1])
+            if groups[0]:
+                byte1 = int(groups[0])
+            if groups[1]:
+                byte2 = int(groups[1])
 
-            chunk, start, length, file_size = get_chunk(
-                f'{prefix}{file}', byte1, byte2)
-            resp = Response(chunk, 206, mimetype='video/mp4',
-                            content_type='video/mp4', direct_passthrough=True)
-            resp.headers.add(
-                'Content-Range', 'bytes {0}-{1}/{2}'.format(start, start + length - 1, file_size))
-            return resp
+        chunk, start, length, file_size = get_chunk(
+            os.path.join(prefix, file), byte1, byte2)
+        resp = Response(chunk, 206, mimetype='video/mp4',
+                        content_type='video/mp4', direct_passthrough=True)
+        resp.headers.add(
+            'Content-Range', 'bytes {0}-{1}/{2}'.format(start, start + length - 1, file_size))
+        return resp
 
 
 def time2str(sec: float) -> str:
-    return str(timedelta(seconds=sec))[:-3]
+    time_str = str(timedelta(seconds=sec))
+    return time_str+'.000' if (sec-int(sec)) == 0 else time_str[:-3]
 
 
 def str2time(string: str) -> float:
@@ -91,54 +93,53 @@ def str2time(string: str) -> float:
 
 @app.route('/api/label', methods=['GET', 'PUT'])
 def api_label():
-    match request.method:
-        case 'GET':
-            with open('./labels.json') as f:
-                bytes = f.read()
-            return Response(bytes,  mimetype='application/json')
-        case 'PUT':
-            request_json = request.get_json()
-            with open('./labels.json', 'w+') as f:
-                f.write(json.dumps(request_json))
-            return 'ok'
+    label_path = os.path.join(os.path.dirname(__file__), 'labels.json')
+    if request.method == 'GET':
+        with open(label_path) as f:
+            bytes = f.read()
+        return Response(bytes,  mimetype='application/json')
+    if request.method == 'PUT':
+        request_json = request.get_json()
+        with open(label_path, 'w+') as f:
+            json.dump(request_json, f, ensure_ascii=False)
+        return 'ok'
 
 
 @app.route('/api/videoLabel', methods=['GET', 'PUT'])
 def api_video_label():
-    match request.method:
-        case 'GET':
-            file = f'{prefix}{request.args.get("file", None)}'
-            withoutext, _ = os.path.splitext(file)
-            label_file = f'{withoutext}.txt'
-            if not os.path.exists(label_file):
-                return Response('[]',  mimetype='application/json')
-            data = []
-            with open(f'{withoutext}.txt', 'r+') as f:
-                while True:
-                    a, b, c = [f.readline() for _ in range(3)]
-                    if len(a) == 0:
-                        break
-                    pattern = r'^(\d+:\d\d:\d\d\.\d\d\d) - (\d+:\d\d:\d\d\.\d\d\d)'
-                    match = re.match(pattern, b)
-                    item = {
-                        'label': a[:-1],
-                        'range': [str2time(match.group(i)) for i in (1, 2)]
-                    }
-                    data.append(item)
-            return Response(json.dumps(data),  mimetype='application/json')
+    if request.method == 'GET':
+        file = os.path.join(prefix, '.'+request.args.get("file", None))
+        withoutext, _ = os.path.splitext(file)
+        label_file = f'{withoutext}.txt'
+        if not os.path.exists(label_file):
+            return Response('[]',  mimetype='application/json')
+        data = []
+        with open(f'{withoutext}.txt', 'r+') as f:
+            while True:
+                a, b, c = [f.readline() for _ in range(3)]
+                if len(a) == 0:
+                    break
+                pattern = r'^(\d+:\d\d:\d\d\.\d\d\d) - (\d+:\d\d:\d\d\.\d\d\d)'
+                match = re.match(pattern, b)
+                item = {
+                    'label': a[:-1],
+                    'range': [str2time(match.group(i)) for i in (1, 2)]
+                }
+                data.append(item)
+        return Response(json.dumps(data),  mimetype='application/json')
 
-        case 'PUT':
-            request_json = request.get_json()
-            key: str = request_json['key']
-            withoutext, _ = os.path.splitext(f'{prefix}{key}')
-            data = request_json['data']
-            with open(f'{withoutext}.txt', 'w+') as f:
-                for item in data:
-                    time_range: List[str] = list(map(time2str, item['range']))
-                    label: str = item['label']
-                    f.write(f'{label}\n{time_range[0]} - {time_range[1]}\n\n')
-            return "ok"
+    if request.method == 'PUT':
+        request_json = request.get_json()
+        key: str = request_json['key']
+        withoutext, _ = os.path.splitext(os.path.join(prefix, '.'+key))
+        data = request_json['data']
+        with open(f'{withoutext}.txt', 'w+') as f:
+            for item in data:
+                time_range: List[str] = list(map(time2str, item['range']))
+                label: str = item['label']
+                f.write(f'{label}\n{time_range[0]} - {time_range[1]}\n\n')
+        return "ok"
 
 
 if __name__ == '__main__':
-    app.run(port=3333)
+    app.run(port=port)
